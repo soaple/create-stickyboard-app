df0a58727ddcfc3fcbfc01df007f5d291c98e97a
Soaple <inje@soaple.io>
2020-07-28 23:49:25 +0900

    PageBase bug fixed.

--- src/templates/stickyboard-simple/src/components/base/PageBase.js
+++ src/templates/stickyboard-simple/src/components/base/PageBase.js
@@ -25,10 +25,6 @@ import { Board, Sticker } from '@stickyboard/core';
 
 import StickerListByCategory from 'components/sticker';
 
-import ApiManager from 'network/ApiManager';
-import StatusCode from 'network/StatusCode';
-import CookieManager from 'network/CookieManager';
-
 import MessageSnackbar from 'components/ui/MessageSnackbar';
 
 import Const from 'constants/Const';
@@ -83,7 +79,7 @@ class PageBase extends React.Component {
     }
 
     componentDidMount() {
-        this.initializeLayout();
+        this.setInitialLayout();
     }
 
     setInitialLayout = () => {
@@ -91,20 +87,6 @@ class PageBase extends React.Component {
             layout: this.props.initialLayout,
             blocks: this.props.initialBlocks,
         });
-    };
-
-    initializeLayout = () => {
-        const userId = CookieManager.getCookie('userId');
-        if (userId) {
-            this.props.showMessageSnackbar('Loading...');
-            ApiManager.StickyBoard.readUserLayout(
-                userId,
-                window.location.pathname,
-                this.readUserLayoutCallback
-            );
-        } else {
-            this.setInitialLayout();
-        }
     };
 
     handleCloseMenu = () => {
@@ -115,45 +97,15 @@ class PageBase extends React.Component {
         this.setState({ isMenuOpen: true });
     };
 
+    onLayoutChange = (newLayouts) => {
+        this.setState({ layout: newLayouts });
+        console.log(JSON.stringify(newLayouts));
+    };
+
     onSaveLayout = () => {
-        const userId = CookieManager.getCookie('userId');
-        if (userId) {
-            ApiManager.StickyBoard.updateUserLayout(
-                userId,
-                window.location.pathname,
-                JSON.stringify(this.state.layout),
-                JSON.stringify(this.state.blocks),
-                this.updateUserLayoutCallback
-            );
-        }
-    };
-
-    readUserLayoutCallback = (statusCode, response) => {
-        this.props.hideMessageSnackbar();
-        switch (statusCode) {
-            case StatusCode.OK:
-                this.setState({
-                    layout: JSON.parse(response.layout),
-                    blocks: JSON.parse(response.blocks),
-                });
-                break;
-            case StatusCode.NOT_FOUND:
-                this.setInitialLayout();
-                break;
-            default:
-                alert(response.msg);
-                break;
-        }
-    };
-
-    updateUserLayoutCallback = (statusCode, response) => {
-        switch (statusCode) {
-            case StatusCode.OK:
-                break;
-            default:
-                alert(response.msg);
-                break;
-        }
+        const { layout, blocks } = this.state;
+        console.log(layout);
+        console.log(blocks);
     };
 
     render() {
@@ -169,10 +121,7 @@ class PageBase extends React.Component {
                 <Board
                     ref={this.board}
                     layouts={layout}
-                    onLayoutChange={(newLayouts) => {
-                        this.setState({ layout: newLayouts });
-                        console.log(JSON.stringify(newLayouts));
-                    }}
+                    onLayoutChange={this.onLayoutChange}
                     onSaveLayout={this.onSaveLayout}>
                     {blocks.map((block, index) => {
                         const StickerObject = StickerDict[block.i];
